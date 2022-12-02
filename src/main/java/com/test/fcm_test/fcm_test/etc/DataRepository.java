41a31cbde122dd1250ce65c6726cfb5274c0ac05
seunghyunyyy <seunghyunyyy@kakao.com>
2022-12-02 11:29:19 +0900

    221202 푸시서버 1차 정리

--- src/main/java/com/test/fcm_test/fcm_test/etc/DataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test.fcm_test.fcm_test.etc;
-/*
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DataRepository extends JpaRepository<DataEntity, Long> {
-}
-*/
--- src/main/java/com/test/fcm_test/fcm_test/etc/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test.fcm_test.fcm_test.etc;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-/*
-public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
-}
-*/
